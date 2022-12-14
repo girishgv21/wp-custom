635454530a7abb8d252ded2fd4256b7eba8ee7cd
girishgv21 <girishgv21@gmail.com>
2022-12-14 08:45:25 +0530

    First Commit

---
+++ functions.php
@@ -0,0 +1,90 @@
+<?php
+function wporg_filter_title( $title ) {
+	return 'Start ' . $title . ' End';
+}
+add_filter( 'the_content', 'wporg_filter_title' );
+
+function mytheme_customize_register( $wp_customize ) {
+	//All our sections, settings, and controls will be added here
+	$wp_customize->add_setting( 'header_textcolor' , array(
+		'default'   => '#888',
+		'transport' => 'refresh',
+	) );
+
+	$wp_customize->add_section( 'mytheme_new_section_name' , array(
+		'title'      => __( 'Visible Section Name', 'mytheme' ),
+		'priority'   => 30,
+	) );
+
+	$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'link_color', array(
+		'label'      => __( 'Header Color', 'mytheme' ),
+		'section'    => 'your_section_id',
+		'settings'   => 'your_setting_id',
+	) ) );
+ }
+ add_action( 'customize_register', 'mytheme_customize_register' );
+
+ function mytheme_customize_css()
+{
+    ?>
+         <style type="text/css">
+             h3 { color: <?php echo get_theme_mod('header_textcolor', '#fff'); ?>; }
+         </style>
+    <?php
+}
+add_action( 'wp_head', 'mytheme_customize_css');
+
+add_theme_support( 'custom-logo' );
+
+/**
+ * Register a custom post type called "book".
+ *
+ * @see get_post_type_labels() for label keys.
+ */
+function wpdocs_codex_book_init() {
+	$labels = array(
+		'name'                  => _x( 'Books', 'Post type general name', 'textdomain' ),
+		'singular_name'         => _x( 'Book', 'Post type singular name', 'textdomain' ),
+		'menu_name'             => _x( 'Books', 'Admin Menu text', 'textdomain' ),
+		'name_admin_bar'        => _x( 'Book', 'Add New on Toolbar', 'textdomain' ),
+		'add_new'               => __( 'Add New', 'textdomain' ),
+		'add_new_item'          => __( 'Add New Book', 'textdomain' ),
+		'new_item'              => __( 'New Book', 'textdomain' ),
+		'edit_item'             => __( 'Edit Book', 'textdomain' ),
+		'view_item'             => __( 'View Book', 'textdomain' ),
+		'all_items'             => __( 'All Books', 'textdomain' ),
+		'search_items'          => __( 'Search Books', 'textdomain' ),
+		'parent_item_colon'     => __( 'Parent Books:', 'textdomain' ),
+		'not_found'             => __( 'No books found.', 'textdomain' ),
+		'not_found_in_trash'    => __( 'No books found in Trash.', 'textdomain' ),
+		'featured_image'        => _x( 'Book Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'textdomain' ),
+		'set_featured_image'    => _x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+		'remove_featured_image' => _x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+		'use_featured_image'    => _x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+		'archives'              => _x( 'Book archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'textdomain' ),
+		'insert_into_item'      => _x( 'Insert into book', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'textdomain' ),
+		'uploaded_to_this_item' => _x( 'Uploaded to this book', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'textdomain' ),
+		'filter_items_list'     => _x( 'Filter books list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'textdomain' ),
+		'items_list_navigation' => _x( 'Books list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'textdomain' ),
+		'items_list'            => _x( 'Books list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'textdomain' ),
+	);
+
+	$args = array(
+		'labels'             => $labels,
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => true,
+		'rewrite'            => array( 'slug' => 'book' ),
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => null,
+		'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
+	);
+
+	register_post_type( 'book', $args );
+}
+
+add_action( 'init', 'wpdocs_codex_book_init' );
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    if (have_posts()) {
+        if ( is_user_logged_in() ) :
+            echo 'Welcome, registered user!';
+        else :
+            echo 'Welcome, visitor!';
+        endif;
+        while (have_posts()) {
+            the_post();
+            ?>
+            <div>
+                <h4><?php the_title(); ?></h4>
+                <div><?php the_content(); ?></div>
+            </div>
+            <?php
+        }
+    }
+
+    $response = wp_remote_get( 'https://wptavern.com/wp-json/wp/v2/posts' );
+    ?>
+
+    <?php wp_footer(); ?>
+</body>
+</html>
